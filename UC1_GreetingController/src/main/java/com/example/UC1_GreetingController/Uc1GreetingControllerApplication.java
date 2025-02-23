8fb9da77a4b7f4a75cc99722333237ab16571e97
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-02-23 23:00:16 +0530

    [KAPIL TIWARI] Added UC1_GreetingController

---
+++ UC1_GreetingController/src/main/java/com/example/UC1_GreetingController/Uc1GreetingControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.UC1_GreetingController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc1GreetingControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc1GreetingControllerApplication.class, args);
+	}
+
+}
